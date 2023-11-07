7c53821ebb109a35530bb3491995e30fa046bc0d
Mugao-joy <mugaojoy@gmail.com>
2023-11-07 17:16:41 +0300

    challenge 2-3

---
+++ challenge2-3.js
@@ -0,0 +1,17 @@
+const item1 = document.querySelector('.list-title1')
+const item2 = document.querySelector('.list-title2')
+const item3 = document.querySelector('.list-title3')
+
+item1.addEventListener('click', function(){
+    console.log('Item 1 was clicked')
+})
+
+item2.addEventListener('click', function(){
+    console.log('Item 2 was clicked')
+})
+
+item3.addEventListener('click', function(){
+    console.log('Item 3 was clicked')
+})
+
+
